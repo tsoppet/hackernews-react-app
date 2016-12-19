abbacfb6bf2f5802ec683e2e7f829bd6bc9a0aaf
timothy-soppet <timothy.soppet@workday.com>
2016-12-18 23:35:49 -0800

    Initial Commit of Hackernews Project with Basic Search of Static List
    Inital commit to HackerNews react project. Includes a basic
    implementation of search via a form component against a static list.

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+function isSearched(query) {
+  return function(item) {
+    return !query || item.title.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+  }
+}
+
+// also in ES6, more functional syntax
+// const isSearched = (query) => (item) => !query || item.title.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list,
+      query: '',
+    };
+
+    this.onSearchChange = this.onSearchChange.bind(this);
+  }
+
+  onSearchChange(event) {
+    this.setState({query: event.target.value});
+  }
+
+  render() {
+    const query = this.state.query;
+    return (
+        <div className="App">
+          <form>
+            <input type="text" value={query} onChange={this.onSearchChange}/>
+          </form>
+          { this.state.list.filter(isSearched(query)).map((item) =>
+              <div key={item.objectID}>
+                <span><a href={item.url}>{item.title}</a></span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+              </div>
+          )}
+        </div>
+    );
+  }
+}
+
+export default App;
